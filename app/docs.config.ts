fc46d6389ce48e284ddcbf98ce3dcad37362dba3
Donkoko <nbonev@duck.com>
2024-02-09 20:07:51 +0200

    reset branch to correct commit

--- app/docs.config.ts
+++ app/docs.config.ts
@@ -1,34 +1,31 @@
 export default {
-    base: '/',
-    lang: 'en-US',
-    title: 'Remix Docs',
-    description: 'Just playing around.',
-    publicURL: 'http://localhost:300',
-    nav: [
-        { text: 'Docs', link: '/docs' },
-        { text: 'Blog', link: '/blog' },
-    ],
-    head: [
-
-    ],
-    sidebar: [
-        {
-            title: 'Documentation',
-            links: [
-                { title: 'Introduction', href: '/docs' },
-                { title: 'Getting started', href: '/docs/getting-started' },
-                { title: 'Asset search', href: '/docs/asset-search' },
-                { title: 'Contribute', href: '/docs/contribute' },
-            ],
-        },
-
-    ],
-    search: {
-        enabled: true,
+  base: "/",
+  lang: "en-US",
+  title: "Remix Docs",
+  description: "Just playing around.",
+  publicURL: "http://localhost:300",
+  nav: [
+    { text: "Docs", link: "/docs" },
+    { text: "Blog", link: "/blog" },
+  ],
+  head: [],
+  sidebar: [
+    {
+      title: "Documentation",
+      links: [
+        { title: "Introduction", href: "/docs" },
+        { title: "Getting started", href: "/docs/getting-started" },
+        { title: "Asset search", href: "/docs/asset-search" },
+        { title: "Contribute", href: "/docs/contribute" },
+      ],
     },
-    editLink: {
-        enabled: true,
-        link: 'https://github.com/Shelf-nu/shelf.nu',
-        text: 'Edit this page on GitHub',
-    },
+  ],
+  search: {
+    enabled: true,
+  },
+  editLink: {
+    enabled: true,
+    link: "https://github.com/Shelf-nu/shelf.nu",
+    text: "Edit this page on GitHub",
+  },
 };
