efc7f43fa80750ffd28ef4c5666754b9d03e97dc
hunar <hunar@netling.digital>
2024-02-14 14:31:16 +0530

    configured dark mode and light mode switching and storing user preffered theme

--- app/components/layout/Container.tsx
+++ app/components/layout/Container.tsx
@@ -31,7 +31,7 @@ export default function Container({ children }) {
 
   const toggleTheme = () => {
     setTheme((prevTheme) =>
-      prevTheme === Theme.LIGHT ? Theme.LIGHT : Theme.LIGHT
+      prevTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
     );
   };
 
@@ -70,6 +70,39 @@ export default function Container({ children }) {
         </div>
         <div className="-my-5 mr-6 sm:mr-8 md:mr-0"></div>
         <div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
+          <button
+            onClick={toggleTheme}
+            aria-label="Toggle Dark Mode"
+            type="button"
+            className="w-8 h-8 bg-slate-400 rounded-full  flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              className="w-6 h-6 text-gray-200 dark:text-gray-200"
+            >
+              <Themed
+                dark={
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                }
+                light={
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  />
+                }
+              />
+            </svg>
+          </button>
           {config.search.enabled && (
             <>
               <button onClick={() => setIsSearching(!isSearching)}>
@@ -96,35 +129,6 @@ export default function Container({ children }) {
               )}
             </>
           )}
-          {/* <button
-                        onClick={toggleTheme}
-                        aria-label="Toggle Dark Mode"
-                        type="button"
-                        className="w-8 h-8 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center  hover:ring-2 ring-gray-300  transition-all"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            className="w-5 h-5 text-gray-800 dark:text-gray-200"
-                        >
-                            <Themed
-                                dark={<path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                                />}
-                                light={<path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                                />}
-                            />
-                        </svg>
-                    </button> */}
         </div>
       </header>
       <div className="relative mx-auto flex max-w-8xl justify-center sm:px-2 lg:px-8 xl:px-12 bg-white dark:bg-gray-800">
